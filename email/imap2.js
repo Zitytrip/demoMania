9309df2153101442dc87ff2b9ebf85a7d8826501
awb99 <andreas.wolfgang.bauer@gmail.com>
2017-10-09 19:03:37 +0200

    email to added

--- email/imap2.js
+++ email/imap2.js
@@ -12,6 +12,55 @@ var client = new ImapClient('imap.gmail.com', 993, {
     }
 });
 
+
+function parseMessage (message) {
+
+    console.log(message.envelope);
+     var data = {
+            date: message.envelope.date,
+            from: message.envelope.from[0].address,
+            to: message.envelope.to[0].address,
+            id: message.envelope["message-id"]
+     };
+
+     var parser = new MimeParser ();
+
+     parser.onbodystructure = function(chunk){
+           console.log("BODY STRUCTURE");
+           console.log(chunk);
+     };
+
+     parser.onbody = function(node, chunk){
+           console.log("BODY");
+           var s = `Received ${chunk.byteLength} bytes for ${node.path.join('.')}`;
+           console.log(s);           
+              
+           var string = new TextDecoder().decode(chunk);
+           //console.log(string);
+            fs.writeFile("/tmp/test.html", string, function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    console.log("The file was saved!");
+            }); 
+               
+            var text = htmlToText.fromString(string, { wordwrap: 130 });
+            console.log(text);
+            data.text = text;
+      };
+
+      parser.onheader = function(node){
+            console.log("HEADER");
+            console.log((node.header.join('\n')).green); // List all headers
+            console.log(node.headers['content-type']); // List value for Content-Type
+      };
+
+      var body = message["body[]"];
+      parser.write(body);
+      parser.end();
+      return data;
+}
+
 async function demo () {
 
     await client.connect();
@@ -23,51 +72,12 @@ async function demo () {
 
     messages.forEach( message => {
         console.log('Flags for ' + message.uid + ': ' + message.flags.join(', '));
-       
-        var body = message["body[]"];
-        delete message["body[]"];
-        console.log("MESSAGE: " + JSON.stringify(message));
+        
+        //delete message["body[]"];
+        //console.log("MESSAGE: " + JSON.stringify(message));
       
-        var data = {
-            date: message.envelope.date,
-            from: message.envelope.from[0].address,
-            id: message.envelope["message-id"]
-        };
+        var data = parseMessage (message);
         console.log(data);
-
-            var parser = new MimeParser ();
-
-            parser.onbodystructure = function(chunk){
-                console.log("BODY STRUCTURE");
-                console.log(chunk);
-            };
-
-            parser.onbody = function(node, chunk){
-                console.log("BODY");
-                var s = `Received ${chunk.byteLength} bytes for ${node.path.join('.')}`;
-                console.log(s);           
-              
-                var string = new TextDecoder().decode(chunk);
-                //console.log(string);
-                fs.writeFile("/tmp/test.html", string, function(err) {
-                    if(err) {
-                        return console.log(err);
-                    }
-                    console.log("The file was saved!");
-                }); 
-               
-                var text = htmlToText.fromString(string, { wordwrap: 130 });
-                console.log(text);
-            };
-
-            parser.onheader = function(node){
-                console.log("HEADER");
-                console.log((node.header.join('\n')).green); // List all headers
-                console.log(node.headers['content-type']); // List value for Content-Type
-            };
-
-            parser.write(body);
-            parser.end();
     });
 
     await client.logout();
